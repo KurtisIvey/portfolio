b860ac73c835b612ebb8eff2ab0aa88989ff1e52
Kitsunebackfire <kwolz92@gmail.com>
2022-08-02 17:03:03 -0500

    add styling to header, fancyName, and navbar to keep it stable and even

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FancyName from "../FancyName/FancyName.js";
 import Navbar from "../Navbar/Navbar.js";
+import "./Header.css";
 
 const Header = () => {
   return (
