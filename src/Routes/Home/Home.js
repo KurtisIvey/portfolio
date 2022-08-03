5b74d7604268dd7cb7d997c69e36bdc307246f3c
Kitsunebackfire <kwolz92@gmail.com>
2022-08-03 15:54:35 -0500

    add skeleton to home.js and add text elements

--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -7,6 +7,24 @@ function App() {
   return (
     <div className="home">
       <Header />
+      <div className="home__body">
+        <div className="home__hiContainer">
+          <div className="home__smallText">Hi, my name is </div>
+          <div className="home__bigText">Kurtis Ivey,</div>
+          <div className="home__medText">and I design websites</div>
+          <div className="home__statement">
+            I'm a self taught front end developer that specializes in using
+            React Js to build interactive websites. Currently, I'm dedicating my
+            time to learning more about web development through various such as
+            The Odin Project and Freecodecamp.
+          </div>
+        </div>
+
+        <img
+          alt="blank headshot"
+          src="https://images.squarespace-cdn.com/content/v1/55778247e4b0e3cc12fde995/1534835065769-MRGCVFIZBLAF0P70RCOK/headshot%2Bblank.png"
+        />
+      </div>
     </div>
   );
 }
